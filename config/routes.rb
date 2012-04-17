c3f004c645418a5e8950b259ba2b7c86c5bb06c7
Tyler Long <tyler4long@gmail.com>
2012-04-17 06:46:49 -0700

    Clear routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 RailsSetup::Application.routes.draw do
-  get "static_pages/home"
-
-  get "static_pages/about"
-
-  get "static_pages/contact"
-
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
