d7470076ab1d65be0c880831f4e7814507fa2623
Tyler Long <tyler4long@gmail.com>
2012-04-17 03:46:53 -0700

    Static pages setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 RailsSetup::Application.routes.draw do
+  get "static_pages/home"
+
+  get "static_pages/about"
+
+  get "static_pages/contact"
+
+  root to: 'static_pages#home'
+  match '/about', to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
