8bd704169a39ae2b02fadfd44539ff08ae242395
Tyler Long <tyler4long@gmail.com>
2012-04-17 19:59:17 -0700

    Add a back page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ RailsSetup::Application.routes.draw do
   root to: 'static_pages#home'
   match '/about', to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
+  match '/back', to: 'static_pages#back'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
